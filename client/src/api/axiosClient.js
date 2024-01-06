cdff72cb786174900fe750b129af15e32b969c5b
Hoang Long <70263014+hoanglong2311@users.noreply.github.com>
2024-01-06 08:28:39 +0700

    Config axios for project

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios  from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://127.0.0.1/api/v1";
+const baseURL = "https://127.0.0.1:5000/api/v1";
 const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
@@ -10,5 +10,25 @@ const axiosClient = axios.create({
 });
 
 axiosClient.interceptors.request.use(async (config) => {
-
+    return{
+        ...config,
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': `Bearer ${getToken()}`
+        }
+    }
 });
+
+axiosClient.interceptors.response.use((response) => {
+    if(response && response.data){
+        return response.data;
+    }
+    return response;
+}, (error) => {
+    if (!error.response) {
+        return alert("Network error, please try again later");
+    }
+    return error.response;
+});
+
+export default axiosClient;
