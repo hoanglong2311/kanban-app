ca685124d96f83b451c59811411798e331d12bc0
Hoang Long <70263014+hoanglong2311@users.noreply.github.com>
2024-01-09 07:14:50 +0700

    Login pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import {Box, TextField} from "@mui/material";
+import {LoadingButton} from "@mui/lab";
 
 const Login = () => {
+    const [loading, setLoading] = React.useState(false);
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <Box
+                component='form'
+                sx={{mt:1}}
+                onSubmit={handleSubmit}
+                noValidate
+            >
+                <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    id='username'
+                    label='Username'
+                    name='username'
+                    disabled={loading}
+                />
+                <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    id='password'
+                    label='Password'
+                    name='password'
+                    disabled={loading}
+                />
+                <LoadingButton
+                sx={{mt: 3,mb:2}}
+                fullWidth
+                variant={'outlined'}
+                color={'success'}
+                type={'submit'}
+                loading={loading}
+                >
+                    Login
+                </LoadingButton>
+            </Box>
+        </>
     );
 };
 
